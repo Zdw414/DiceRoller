4500b3c80177b8ec569aff3bcbec62f5c93f62cd
Zdw414 <zdw10030@saintjoe.edu>
2015-02-16 23:39:46 -0500

    initial commit

---
+++ DiceRoller.js
@@ -0,0 +1,10 @@
+        //Zach Dickinson DiceRoller Code
+        // 2/8/14
+var dice1=Math.floor(Math.random()*6);
+var dice2=Math.floor(Math.random()*6);
+dice1 += 1;
+dice2 += 1;
+console.log(dice1) ;
+console.log(dice2);
+
+
